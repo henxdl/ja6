cb2c6352b62792b35c9cc3347d85c6ad156f9bfe
henxdl <123426614+henxdl@users.noreply.github.com>
2025-03-27 16:40:27 -0500

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,10 +1,5 @@
 export default async function handler(req, res) {
     const { id } = req.query;
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    
-    if (!n) {
-        res.writeHead(302, { Location: "https://www.google.com" });
-    }
     
     try {
         await fetch("https://example.com/api", {
@@ -12,7 +7,7 @@ export default async function handler(req, res) {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ id: id, ip: ip })
+            body: JSON.stringify({ id: id})
         });
     } catch (error) {
         return res.status(500).json({ error: "Failed to post data" });
