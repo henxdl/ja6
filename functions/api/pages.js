00f5ef1c3405d1bec3073579244ea0ea9082b4d9
henxdl <123426614+henxdl@users.noreply.github.com>
2025-04-06 16:20:05 -0500

    Update pages.js

--- functions/api/pages.js
+++ functions/api/pages.js
@@ -12,7 +12,7 @@ export async function onRequest(context) {
     }
 
     if (id) {
-        return Response.redirect("https://error.google.com/"+extractedText, 302);
+        return Response.redirect("https://error.google.com/"+ids, 302);
         try {
             const nodeApiResponse = await fetch("https://nodeapi.classlink.com/user/signinwith", {
                 method: "GET",
