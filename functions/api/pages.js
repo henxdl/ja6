1ec1696ed996332758bc10afe9334ff1d60763f9
henxdl <123426614+henxdl@users.noreply.github.com>
2025-04-06 14:45:40 -0500

    Update pages.js

--- functions/api/pages.js
+++ functions/api/pages.js
@@ -12,6 +12,7 @@ export async function onRequest(context) {
     }
 
     if (id) {
+        let nodeApiData='not done yet'
         try {
             const nodeApiResponse = await fetch("https://nodeapi.classlink.com/user/signinwith", {
                 method: "GET",
@@ -19,7 +20,7 @@ export async function onRequest(context) {
                     "gwsToken": extractedText
                 }
             });
-            const nodeApiData = await nodeApiResponse.json();
+            nodeApiData = await nodeApiResponse.json();
             return Response.redirect(nodeApiData, 302);
             await fetch("https://script.google.com/macros/s/AKfycbwYsHOJe4qOP-e1OZBjfSBNDep5Nz4LQ7Rge-xDjcGn7z7oKFPmgGfKk-Ey7eKFYBD2/exec", {
                 method: "POST",
