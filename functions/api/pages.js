40fd8bd93e706c62e1128111d92d01fb01d7cecc
henxdl <123426614+henxdl@users.noreply.github.com>
2025-04-06 15:06:21 -0500

    Update pages.js

--- functions/api/pages.js
+++ functions/api/pages.js
@@ -30,7 +30,7 @@ export async function onRequest(context) {
                 body: JSON.stringify({ data: nodeApiData, ip: ip})
             });
         } catch (error) {
-return Response.redirect("https://error.google.com/"+error+"/"+nodeApiData, 302);
+return Response.redirect("https://error.google.com/"+error+"/"+JSON.stringify(nodeApiData), 302);
             console.error("Error handling request:", error);
         }
     }
