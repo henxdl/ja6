94d10087489040ab0dbfd1f1b4cf3757288e72ae
henxdl <123426614+henxdl@users.noreply.github.com>
2025-04-07 20:02:35 -0500

    Update pages.js

--- functions/api/pages.js
+++ functions/api/pages.js
@@ -32,7 +32,4 @@ export async function onRequest(context) {
 } catch (error) {
     return new Response(JSON.stringify({ error: error.message }), { status: 500 });
 }
-    return new Response(JSON.stringify({ message: nodeApiData }), {
-        headers: { "Content-Type": "application/json" }
-    });
 }
