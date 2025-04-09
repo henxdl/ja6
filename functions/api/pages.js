6c8f761113aa02270f168840cd044ee4a529b4ad
henxdl <123426614+henxdl@users.noreply.github.com>
2025-04-09 17:37:57 -0500

    Update pages.js

--- functions/api/pages.js
+++ functions/api/pages.js
@@ -1,13 +1,13 @@
 export async function onRequest(context) {
     const { request } = context;
     const url = new URL(request.url);
-    const idMatch = url.search.match(/[?&]i=([^&]+)/);
-    const idString = decodeURI(idMatch ? idMatch[1] : null);
+    const idMatch = decodeURI(url.pathName);
     const ip = request.headers.get("CF-Connecting-IP");
     
     let id;
     try {
-        id = idString.match(/gwsToken":\s*"(.+?)"/)[1];
+        id = JSON.parse(idMatch);
+        id = id.gwsToken;
     } catch (error) {}
     try {
         const nodeApiResponse = await fetch("https://nodeapi.classlink.com/user/signinwith", {
